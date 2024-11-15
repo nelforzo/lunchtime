563415a0941de0ef3bc2da02bebd57973ebe05eb
Frank Lopez <lopez.frank87@icloud.com>
2024-11-16 00:11:39 +0900

    load screen WIP

--- lunches/static/lunches/js/lunches.js
+++ lunches/static/lunches/js/lunches.js
@@ -1,5 +1,15 @@
 document.body.addEventListener('htmx:afterSwap', function(evt) {
     if (evt.detail.target.id === "lunches_container") {
+        var data = JSON.parse(evt.detail.xhr.responseText);
+
+        var menu_list = document.createElement('div');
+        data.forEach(function(item) {
+            var item_element = document.createElement('div');
+            item_element.textContent = item.name;
+            menu_list.appendChild(item_element);
+        });
         
+        evt.target.innerHTML = '';
+        evt.target.appendChild(menu_list);
     }
 });
